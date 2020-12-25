2d56a0d3fcdfc5fc3a6dc9f1be8c8c5fde948b63
JOHN <xiaomozi_2016@outlook.com>
2020-12-25 15:38:21 +0800

    Update meshreduce-houdini.py

--- meshreduce-houdini.py
+++ meshreduce-houdini.py
@@ -5,24 +5,32 @@ class FbxMeshReduce:
     def __init__(self,kwargs):
         option = kwargs
         self.nodes={}
-        self.inputFbxFile = option['inputFbxFile']
-        self.compressionRate = option['compressionRate'] or 70 
-        self.outputBaker = option['outputBaker']
-        self.outputFbx = option['outputFbx']
-        self.attachTexture = option['attachTexture']
+        self.inputFbxFile = self._checkValue(option['inputFbxFile'])
+        self.compressionRate = self._checkValue(option['compressionRate']) or 70 
+        self.outputBaker = self._checkValue(option['outputBaker'])
+        self.outputFbx = self._checkValue(option['outputFbx'])
+        self.attachTexture = self._checkValue(option['attachTexture'])
+        self.outputObj = self._checkValue(option['outputObj'])
+        self.baker = self._checkValue(option['baker'])
         
         try:
             import hou
             
         except ImportError:
             
-            raise Exception('import hou error')
+            print('import hou error')
         
         self.compress()
         
         pass
 
 
+    def _checkValue(self,value):
+        if value == "None":
+            return None
+        else:
+            return value
+        
     def _importFbx(self,attachTexture = None):
         try:
             
@@ -45,7 +53,8 @@ class FbxMeshReduce:
             
             
         except Exception:
-            raise Exception('fail to import {0}'.format(self.inputFbxFile))
+            #raise Exception('fail to import {0}'.format(self.inputFbxFile))
+            print('fail to import {0}'.format(self.inputFbxFile))
         
 
     def _createNodes(self):
@@ -54,19 +63,30 @@ class FbxMeshReduce:
         uv = geo.createNode('uvlayout','uvlayout2')
         material2 = geo.createNode('material','material2')
         baker = geo.createNode('maps_baker','maps_baker1')
+        
+        #default baker:not baker
+        compressSwitch  = geo.createNode('switch','switch2')
+        
         fbxout = geo.createNode('rop_fbx','rop_fbx1')
         clean = geo.createNode('clean','clean1')
         fusePoly = geo.createNode('fuse','fuse2')
+
+        normal  = geo.createNode('normal','normal2')
+        clean3  = geo.createNode('clean','clean3')
+        rop_geometry2  = geo.createNode('rop_geometry','rop_geometry2')
 
         self.nodes['polyreduce'] =polyreduce
         self.nodes['uv'] =uv
         self.nodes['material2'] =material2
         self.nodes['baker'] =baker
+        self.nodes['compressSwitch']=compressSwitch
         self.nodes['fbxout'] =fbxout
         self.nodes['clean'] =clean
         self.nodes['fusePoly'] = fusePoly
 
-        pass
+        self.nodes['normal'] = normal
+        self.nodes['clean3'] = clean3
+        self.nodes['rop_geometry2'] = rop_geometry2
 
     def _connectNodes(self):
 
@@ -75,8 +95,16 @@ class FbxMeshReduce:
         self.nodes['baker'].setFirstInput(self.nodes['uv'])
         self.nodes['baker'].setNextInput(self.nodes['material'])
         self.nodes['clean'].setFirstInput(self.nodes['baker'])
-        self.nodes['fusePoly'].setFirstInput(self.nodes['clean'])
+        
+        self.nodes['compressSwitch'].setFirstInput(self.nodes['clean'])
+        self.nodes['compressSwitch'].setNextInput(self.nodes['material'])
+
+        self.nodes['fusePoly'].setFirstInput(self.nodes['compressSwitch'])
         self.nodes['polyreduce'].setFirstInput(self.nodes['fusePoly'])
+        
+        self.nodes['normal'].setFirstInput(self.nodes['polyreduce'])
+        self.nodes['clean3'].setFirstInput(self.nodes['normal'])
+        self.nodes['rop_geometry2'].setFirstInput(self.nodes['clean3'])
 
         self.nodes['fbxout'].setFirstInput(self.nodes['polyreduce'])
         pass
@@ -88,18 +116,39 @@ class FbxMeshReduce:
         baker.parm('bDiffuse').set(1)
         baker.parm('bVertexCd').set(0)
         baker.parm('bAO').set(0)
+        #--------------------switch
+        if self.baker:
+           self.nodes['compressSwitch'].parm('input').set(1) 
+        
         #--------------------
         polyreduce = self.nodes['polyreduce']
         polyreduce.parm('percentage').set(self.compressionRate)
+        #-------------------
+        self.nodes['clean3'].parm('dodelattribs').set(1)
+        self.nodes['clean3'].parm('delattribs').set('* ^P ^uv')
+        self.nodes['clean3'].parm('dodelgroups').set(1)
+        #--------------------
+        rop_geometry2 = self.nodes['rop_geometry2']
+        rop_geometry2.parm('sopoutput').set(self.outputObj)
         #--------------------
         fbxout = self.nodes['fbxout']
         fbxout.parm('sopoutput').set(self.outputFbx)
         pass
 
     def _run(self):
-        self.nodes['baker'].parm('execute').pressButton()
-        self.nodes['fbxout'].parm('execute').pressButton()
-        pass
+        
+        if self.outputBaker:
+            print('baking {0}.......'.format(self.outputBaker))
+            self.nodes['baker'].parm('execute').pressButton()
+            
+        if self.outputFbx:
+            print('output Fbx file ...{0}'.format(self.outputFbx))
+            self.nodes['fbxout'].parm('execute').pressButton()
+            
+        if self.outputObj:
+            print('output OBJ file ...{0}'.format(self.outputObj))
+            self.nodes['rop_geometry2'].parm('execute').pressButton()
+        
 
     def compress(self):
         try:
@@ -110,7 +159,7 @@ class FbxMeshReduce:
             self._run()
         except Exception :
             print('compress failed')
-            raise Exception("compress failed")
+            #raise Exception("compress failed")
         
 
 if __name__ == '__main__':
@@ -118,18 +167,21 @@ if __name__ == '__main__':
     import argparse
     ap = argparse.ArgumentParser()
     ap.add_argument("-i", "--inputFbxFile", required=True,help="input Fbx filename")
-    ap.add_argument("-o", "--outputFbx", required=True,help="output Fbx filename")
-    ap.add_argument("-oj", "--outputJPEG", required=True,help="output baker filename")
+    ap.add_argument("-o", "--outputFbx", required=False,help="output Fbx filename")
+    ap.add_argument("-oj", "--outputJPEG", required=False,help="output baker filename")
     ap.add_argument("-r", "--compressionRate", required=True,help="reduction to the num of compressionRate")
     ap.add_argument("-a", "--attachTexture", required=False,help="attachTexture")
+    ap.add_argument("-obj", "--outputObj", required=False,help="save files in OBJ fileformat")
+    ap.add_argument("-ba", "--baker", required=False,help="baker")
     args = vars(ap.parse_args())
-        
+    
     inputFbxFile = args['inputFbxFile']
     compressionRate = args['compressionRate']
     outputBaker = args['outputJPEG']
     outputFbx = args['outputFbx']
+    outputObj = args['outputObj']
     attachTexture = args['attachTexture']
- 
+    baker = args['baker']
     # options = {
     #     'inputFbxFile':r'F:\code\mygitcollection\houdini\cube.fbx',
     #     'compressionRate':20,
@@ -141,6 +193,8 @@ if __name__ == '__main__':
         'compressionRate':compressionRate or int(compressionRate),
         'outputBaker':r'{0}'.format(outputBaker),
         'outputFbx':r'{0}'.format(outputFbx),
-        'attachTexture':r'{0}'.format(attachTexture)
+        'outputObj':r'{0}'.format(outputObj),
+        'attachTexture':r'{0}'.format(attachTexture),
+        'baker':r'{0}'.format(baker)
     }
     fbx = FbxMeshReduce(options)
